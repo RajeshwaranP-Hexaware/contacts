6d2bdb972bfd23f28cddcb916f1c6ca114a67df1
Rajeshwaran P <rajeshfederer2@gmail.com>
2019-06-12 15:42:54 +0530

    Update Contact

--- handlers/contacts.js
+++ handlers/contacts.js
@@ -1,3 +1,7 @@
+"use strict";
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
 let ContactModel = require('../models/contacts');
 let contacts = {};
 
@@ -14,6 +18,46 @@ contacts.addContact = async (req, res, next) => {
         res.json({ "message": "Contact Added Successfully", "id": createdData._id }).end();
     } catch (e) {
         console.log(e);
+        return next(e);
+    }
+};
+
+/**
+ * Validate id whether mongo object id and returns true or false 
+ */
+const validateId = (id) => {
+    return new Promise((resolve, reject) => {
+        if (!ObjectId.isValid(id)) {
+            return resolve(true);
+        } else {
+            return resolve(false);
+        }
+    })
+}
+
+contacts.updateContact = async (req, res, next) => {
+    try {
+        const reqObj = req.body;
+        let isValidId = await validateId(reqObj.id);
+        if (!isValidId) {
+            res.status(420);
+            res.json({ "message": "Contact Not Found" }).end();
+        }
+        let contactData = await ContactModel.findOne({ _id: mongoose.Types.ObjectId(reqObj.id) })
+        console.log('FEDDF ', contactData);
+        if (contactData) {
+            contactData.name = reqObj.name;
+            contactData.emails = reqObj.emails;
+            contactData.phoneNos = reqObj.phoneNos;
+            await contactData.save();
+            res.status(200);
+            res.json({ "message": "Contact Updated Successfully" }).end();
+        } else {
+            res.status(420);
+            res.json({ "message": "Contact Not Found" }).end();
+        }
+    } catch (e) {
+        console.log('PPPPP ', JSON.stringify(e));
         return next(e);
     }
 };
